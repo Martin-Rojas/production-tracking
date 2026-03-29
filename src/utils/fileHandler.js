5911d3d3dfda40acb9be5cd0e44350e38e99dd60
MartinRojas <rojas.martin25@yahoo.com>
2026-03-28 17:36:27 -0700

    refactor: add file handler utility for           JSON read/write operations

---
+++ src/utils/fileHandler.js
@@ -0,0 +1,19 @@
+import fs from "node:fs";
+
+const loadProduction = () => {
+   try {
+      const dataBuffer = fs.readFileSync(`data/production.json`);
+      const dataJson = dataBuffer.toString();
+      console.log(`it works`);
+      return JSON.parse(dataJson);
+   } catch (error) {
+      return [];
+   }
+};
+
+const saveProductionRun = (productionRun) => {
+   const dataJSON = JSON.stringify(productionRun);
+   fs.writeFileSync(`data/production.json`, dataJSON);
+};
+
+export { loadProduction, saveProductionRun };
